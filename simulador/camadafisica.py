d1eaf209522be1cfda20a2b82e8d2682059af8f8
Manoel Stilpen <mstilpen15@gmail.com>
2017-07-28 15:43:04 -0300

    adicionado nome da Ana

--- simulador/camadafisica.py
+++ simulador/camadafisica.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
     File name: camadafisica.py
-    Author: Daniela Pralon, Eduardo Andrews, João Paulo Reis Alvarenga, Manoel Stilpen, Patrick Rosa
+    Author: Ana Moraes, Daniela Pralon, Eduardo Andrews, João Paulo Reis Alvarenga, Manoel Stilpen, Patrick Rosa
     Date created: 5/30/2017
     Data last modified: 6/30/2017
     Python version: 2.7
