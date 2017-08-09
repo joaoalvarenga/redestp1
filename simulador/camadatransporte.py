d281e68e1c8c398c4bb74833c427ec8d5f3481bf
Manoel Stilpen <mstilpen15@gmail.com>
2017-08-09 10:55:18 -0300

    arquivo camada de transporte

---
+++ simulador/camadatransporte.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    File name: camadatransporte.py
+    Author: Ana Moraes, Daniela Pralon, Eduardo Andrews, João Paulo Reis Alvarenga, Manoel Stilpen, Patrick Rosa
+    Date created: 8/09/2017
+    Data last modified: 8/09/2017
+    Python version: 2.7
+    License: GPL
+"""
+
+class CamadaTransporte(object):
+    """
+    Simulacao da Camada de Transporte
+    """
+
+    def __init__(self):
+        
